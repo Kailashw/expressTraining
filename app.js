c75b42e8cffe7911f8e7bfa8644f65bd11667e19
Kailas Walldoddi <Kailas.Walldoddi@ellucian.com>
2021-07-24 12:32:37 +0530

    updated delete method for user route

--- app.js
+++ app.js
@@ -2,11 +2,45 @@ import { app } from './server';
 import { userRoutes } from "./routes/userRoutes";
 import { productRoutes } from "./routes/productRoutes";
 import path from "path";
-
+import fs from "fs";
 const port = 8080
+import users from "./users.json";
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'static/index.html'));
+})
+
+app.delete('/users', (req, res) => {
+    const { id, name, age } = req.query
+    var usersData;
+    fs.readFile('users.json', function (err,data) {
+        usersData = JSON.parse(data);
+        if (id && usersData.find(el => el.id === Number(id))) {
+            usersData = usersData.filter(user => user.id !== Number(id));
+            fs.writeFile('users.json', JSON.stringify(usersData, null, 4), 'utf-8', function (err) {
+                if (err) throw Error(err);
+                res.status(200).send(`User with id: ${id} successfully deleted`);
+            })
+        } 
+        else if (name && usersData.find(el => el.name === name) ){
+            usersData = usersData.filter(user => user.name !== name);
+            fs.writeFile('users.json', JSON.stringify(usersData, null, 4), 'utf-8', function (err) {
+                if (err) throw Error(err);
+                res.status(200).send(`User with name: ${name} successfully deleted`);
+            })
+        }
+        else if (age && usersData.find(el => el.age === age) ){
+            usersData = usersData.filter(user => user.age !== Number(age));
+            fs.writeFile('users.json', JSON.stringify(usersData, null, 4), 'utf-8', function (err) {
+                if (err) throw Error(err);
+                res.status(200).send(`User with age: ${age} successfully deleted`);
+            })
+        }
+        else {
+            res.status(500).send(`user with given property ${JSON.stringify({id,name,age})} not found.`);
+        }
+    })
+   
 })
 
 productRoutes(app);
@@ -15,7 +49,7 @@ userRoutes(app);
 app.use(userRoutes);
 app.use(productRoutes);
 
-app.get('*',(request,response)=>{
+app.get('*', (request, response) => {
     response.send(`${request.params['0'].split("/")[1].toUpperCase()} route Not found !!`)
 })
 
--- server.js
+++ server.js
@@ -3,4 +3,9 @@ var app = express();
 app.use(express.json())
 app.use(express.static('static'))
 
+process.on('uncaughtException', function(err) {
+    console.log("Uncaught exception!", err);
+    process.exit();
+});
+
 module.exports.app=app;
